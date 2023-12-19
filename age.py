b4f0a4864bd00c9c019d244553077b32abf4d07a
kavyamallik <154292122+kavyamallik@users.noreply.github.com>
2023-12-19 02:26:09 +0000

    if else practice

---
+++ age.py
@@ -0,0 +1,9 @@
+print("enter your age:")
+age_str = input()
+age = int(age_str)
+if age < 13: 
+    print ("You are a kid.")
+elif age < 20:
+    print("You are a teen.")
+else:
+    print("You are an adult.")
